997722b19fd496342ff37b40dde6fd6a422b9894
Vinicius Miranda <vinniimiranda@gmail.com>
2020-11-16 17:54:46 -0300

    add days to request

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import datetime
 from time import sleep
 import os
 
-end_date = datetime.datetime.now()
-days = datetime.timedelta(0)
-start_date = end_date - days
 
 def handle(request):
     ticker = request.rel_url.query.get('ticker')
+    days = request.rel_url.query.get('days') or 0
+    end_date = datetime.datetime.now()
+    sub = datetime.timedelta(days)
+    start_date = end_date - sub
     data = si.get_data(f'{ticker}.SA', start_date, end_date, index_as_date=False, interval="1d")
     return web.Response(text=data.to_csv())
 app = web.Application()
