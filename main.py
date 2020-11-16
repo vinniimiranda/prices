a7c11278a095bfb0aa770a15f1768ab7d2385773
Vinicius Miranda <vinniimiranda@gmail.com>
2020-11-16 18:02:18 -0300

    days as int

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def handle(request):
     ticker = request.rel_url.query.get('ticker')
     days = request.rel_url.query.get('days') or 0
     end_date = datetime.datetime.now()
-    sub = datetime.timedelta(days)
+    sub = datetime.timedelta(int(days))
     start_date = end_date - sub
     data = si.get_data(f'{ticker}.SA', start_date, end_date, index_as_date=False, interval="1d")
     return web.Response(text=data.to_csv())
