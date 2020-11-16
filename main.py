b3097717aa0b3fbeb198da9c307909abf7dea6c0
Vinicius Miranda <vinniimiranda@gmail.com>
2020-11-16 17:19:14 -0300

    add port

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from aiohttp import web
 from yahoo_fin import stock_info as si
 import datetime
 from time import sleep
+import os
 
 end_date = datetime.datetime.now()
 days = datetime.timedelta(0)
@@ -11,8 +12,8 @@ def handle(request):
     ticker = request.rel_url.query.get('ticker')
     data = si.get_data(f'{ticker}.SA', start_date, end_date, index_as_date=False, interval="1d")
     return web.Response(text=data.to_csv())
-
 app = web.Application()
 app.router.add_get('/', handle)
 
-web.run_app(app)
+port = int(os.environ.get('PORT', 8000))
+web.run_app(app, port=port)
