f2baf9348d7c70a6f39280a3f7fc063a400d2d6e
Vinicius Miranda <vinniimiranda@gmail.com>
2020-11-17 16:55:02 -0300

    Update

--- main.py
+++ main.py
@@ -33,6 +33,5 @@ async def handle(websocket, path):
             await websocket.send('FALSE')
 
 start_server = websockets.serve(handle, "127.0.0.1", port)
-
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
